9bae8ce91a6789cb024bfd3ef8c4065c2854d6cf
Mikko Pitkänen <mikko@mbit.fi>
2016-04-21 01:57:16 -0400

    Added BloodSugar, Notes and BloodPressure to LiveQuery

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var api = new ParseServer({
   masterKey: process.env.MASTER_KEY || secrets.masterKey, //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || secrets.serverURL,  // Don't forget to change to https if needed
   liveQuery: {
-    classNames: ["Journal"] // List of classes to support for query subscriptions
+    classNames: ["Journal", "BloodSugar", "BloodPressure", "Notes"] // List of classes to support for query subscriptions
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
